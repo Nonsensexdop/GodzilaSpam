58b84282c9b34180ad09986c2af3068e2bb29bd6
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-12 13:38:41 +0530

    Create __main__.py

---
+++ SpamX/__main__.py
@@ -0,0 +1,349 @@
+#SpamX Spam bot by @GODZILLA_SPAMBOT || @HYPER_AD13
+
+import asyncio
+import sys
+from sys import argv
+import glob
+from pathlib import Path
+from SpamX.utils import load_plugins
+import logging
+from telethon import events
+from . import omfo, omfo2, omfo3, omfo4, omfo5 , omfo6, omfo7, omfo8, omfo9, omfo10, omfo11, omfo12, omfo13, omfo14, omfo15, omfo16, omfo17, omfo18, omfo19, omfo20, omfo21, omfo22, omfo23, omfo24, omfo25, omfo26, omfo27, omfo28, omfo29, omfo30, omfo31, omfo32, omfo33, omfo34, omfo35, omfo36, omfo37, omfo38, omfo39, omfo40
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+path = "SpamX/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("GODZILA Bot Spam Successfully deployed -!")
+print("Enjoy! Do visit @SILENT_DEVS // @GODZILLA_SPAMBOT")
+
+if len(argv) not in (1, 3, 4):
+    try:
+        omfo.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo2.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo3.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo4.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo5.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo6.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo7.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo8.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo9.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo10.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo11.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo12.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo13.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo14.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo15.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo16.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo17.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo18.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo19.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo20.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo21.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo22.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo23.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo24.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo25.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo26.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo27.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo28.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo29.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo30.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo31.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo32.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo33.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo34.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo35.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo36.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo37.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo38.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo39.disconnect()
+    except Exception as e:
+        pass
+    try:
+        omfo40.disconnect()
+    except Exception as e:
+        pass
+else:
+    try:
+        omfo.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo2.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo3.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo4.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo5.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo6.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo7.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo8.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo9.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo10.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo11.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo12.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo13.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo14.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo15.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo16.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo17.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo18.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo19.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo20.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo21.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo22.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo23.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo24.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo25.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo26.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo27.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo28.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo29.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo30.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo31.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo32.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo33.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo34.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo35.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo36.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo37.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo38.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo39.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        omfo40.run_until_disconnected()
+    except Exception as e:
+        pass
